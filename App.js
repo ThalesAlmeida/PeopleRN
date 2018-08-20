43d46896435b2f60604739dc8e9049b0bb1ac4db
Thales <thalesalves44@gmail.com>
2018-08-19 23:52:47 -0300

    mandando o projeto

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Header from './src/components/Header';
+import PeopleList from './src/components/PeopleList'
+
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    //Define um estado para o construtor
+    this.state = {
+      peoples: []
+    };
+  }
+
+      componentDidMount(){
+        // Busca na api os dados dos usuários
+        axios.get('https://randomuser.me/api/?nat=br&results=5').then(response=>{
+          const { results } = response.data;
+          this.setState({
+            // Atribui o results a variável peoples
+            peoples: results
+          })
+        })    
+    }
+
+  render() {
+    return (
+      <View>
+        <Header title="Pessoas!"/>
+        <PeopleList peoples={this.state.peoples} />
+      </View>
+    );
+  }
+}
+
+
