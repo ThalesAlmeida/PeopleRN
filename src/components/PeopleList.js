5455560dca81f28aba33d15069efc2f6b9579e24
Thales <thalesalves44@gmail.com>
2018-10-02 20:15:20 -0300

    tratando erros

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 import PeopleListItem from './PeopleListItem';
 
@@ -7,18 +7,20 @@ import PeopleListItem from './PeopleListItem';
 const PeopleList = props => {
     const { peoples, onPressItem } = props;
 
-    const items = peoples.map(people => {
-        return <PeopleListItem key={people.name.first} people={people} navigateToPeopleDetail={onPressItem} />
-    })
-    
     return (
-    <ScrollView style={styles.container}>
-        { items }
-    </ScrollView>
+        <FlatList
+            style={styles.container}
+            data={peoples}
+            renderItem={({ item }) => (
+                <PeopleListItem
+                    people={item}
+                    navigateToPeopleDetail={onPressItem} />
+            )}
+            keyExtractor={item=>item.name.first} />
     )
 };
 
-const styles = StyleSheet.create( {
+const styles = StyleSheet.create({
     container: {
         backgroundColor: '#e2f9ff'
     },
